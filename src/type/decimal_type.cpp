492059d6416e76e628f1350e8f098709ca8d4ac8
Garrison Hess <garrison.hess@gmail.com>
2022-05-21 01:36:06 -0400

    Remove spurious cast check

--- src/type/decimal_type.cpp
+++ src/type/decimal_type.cpp
@@ -289,7 +289,7 @@ Value DecimalType::CastAs(const Value &val, const TypeId type_id) const {
       if (val.IsNull()) {
         return Value(type_id, BUSTUB_INT32_NULL);
       }
-      if (val.GetAs<double>() > BUSTUB_INT32_MAX || val.GetAs<double>() < BUSTUB_INT32_MIN) {
+      if (val.GetAs<long>() > BUSTUB_INT32_MAX || val.GetAs<double>() < BUSTUB_INT32_MIN) {
         throw Exception(ExceptionType::OUT_OF_RANGE, "Numeric value out of range.");
       }
       return Value(type_id, static_cast<int32_t>(val.GetAs<double>()));
@@ -297,9 +297,6 @@ Value DecimalType::CastAs(const Value &val, const TypeId type_id) const {
     case TypeId::BIGINT: {
       if (val.IsNull()) {
         return Value(type_id, BUSTUB_INT64_NULL);
-      }
-      if (val.GetAs<double>() > BUSTUB_INT64_MAX || val.GetAs<double>() < BUSTUB_INT64_MIN) {
-        throw Exception(ExceptionType::OUT_OF_RANGE, "Numeric value out of range.");
       }
       return Value(type_id, static_cast<int64_t>(val.GetAs<double>()));
     }
