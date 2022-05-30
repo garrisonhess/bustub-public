7fe03824d36e8a495cfa4dbf1487aa3c06c8182a
Garrison Hess <garrison.hess@gmail.com>
2022-05-30 11:03:24 -0400

    checkpoint

--- src/common/printer.cpp
+++ src/common/printer.cpp
@@ -1,6 +1,5 @@
 
 #include "common/printer.h"
-// #include "common/progress_bar.h"
 
 #include <cstdio>
 
@@ -12,22 +11,5 @@ void Printer::Print(const string &str) {
   fprintf(stderr, "%s\n", str.c_str());
 #endif
 }
-
-// void Printer::PrintProgress(int percentage, const char *pbstr, int pbwidth) {
-// #ifndef BUSTUB_DISABLE_PRINT
-// 	int lpad = static_cast<int>(percentage / 100.0 * pbwidth);
-// 	int rpad = pbwidth - lpad;
-// 	printf("\r%3d%% [%.*s%*s]", percentage, lpad, pbstr, rpad, "");
-// 	fflush(stdout);
-// #endif
-// }
-
-// void Printer::FinishProgressBarPrint(const char *pbstr, int pbwidth) {
-// #ifndef BUSTUB_DISABLE_PRINT
-// 	PrintProgress(100, pbstr, pbwidth);
-// 	printf(" \n");
-// 	fflush(stdout);
-// #endif
-// }
 
 }  // namespace bustub
