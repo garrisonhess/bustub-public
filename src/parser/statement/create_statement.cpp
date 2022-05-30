872f8956f9260b7844b9e01b55e3c8389b19e2fd
Garrison Hess <garrison.hess@gmail.com>
2022-05-30 03:10:36 -0400

    tidy and format

--- src/parser/statement/create_statement.cpp
+++ src/parser/statement/create_statement.cpp
@@ -4,8 +4,7 @@ namespace bustub {
 
 CreateStatement::CreateStatement() : SQLStatement(StatementType::CREATE_STATEMENT) {}
 
-CreateStatement::CreateStatement(const CreateStatement &other)
-    : SQLStatement(other), table_(other.table_), schema_(other.schema_), sql_(other.sql_) {}
+CreateStatement::CreateStatement(const CreateStatement &other) = default;
 
 unique_ptr<SQLStatement> CreateStatement::Copy() const {
   return unique_ptr<CreateStatement>(new CreateStatement(*this));
