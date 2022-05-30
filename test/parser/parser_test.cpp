9e07c8c8f930595498051828e61238eb7a6d507a
Garrison Hess <garrison.hess@gmail.com>
2022-05-30 02:14:52 -0400

    Fix create table test and bad query test.

--- test/parser/parser_test.cpp
+++ test/parser/parser_test.cpp
@@ -24,7 +24,6 @@ TEST(ParserTest, SimpleParserTest) {
   parser.ParseQuery("SELECT * FROM test_table;");
 
   for (auto &&stmt : parser.statements_) {
-    LOG_INFO("Parsed statement: %s", stmt->ToString().c_str());
     LOG_INFO("Statement data: query: %s", stmt->query_.c_str());
     LOG_INFO("Statement data: type: %d", static_cast<int>(stmt->type_));
   }
@@ -35,7 +34,6 @@ TEST(ParserTest, CreateParserTest) {
   parser.ParseQuery("CREATE TABLE test_table (column1 int, column2 decimal, column3 varchar);");
 
   for (auto &&stmt : parser.statements_) {
-    LOG_INFO("Parsed statement: %s", stmt->ToString().c_str());
     LOG_INFO("Statement data: query: %s", stmt->query_.c_str());
     LOG_INFO("Statement data: type: %d", static_cast<int>(stmt->type_));
   }
@@ -46,7 +44,6 @@ TEST(ParserTest, InsertParserTest) {
   parser.ParseQuery("INSERT INTO test_table (column1, column2, column3) VALUES (1, 1.0, '1'));");
 
   for (auto &&stmt : parser.statements_) {
-    LOG_INFO("Parsed statement: %s", stmt->ToString().c_str());
     LOG_INFO("Statement data: query: %s", stmt->query_.c_str());
     LOG_INFO("Statement data: type: %d", static_cast<int>(stmt->type_));
   }
@@ -54,9 +51,11 @@ TEST(ParserTest, InsertParserTest) {
 
 TEST(ParserTest, BadQueryParserTest) {
   Parser parser;
-  parser.ParseQuery("SELECT * F_ROM test_table;");
-  for (auto &&stmt : parser.statements_) {
-    LOG_INFO("Parsed statement: %s", stmt->ToString().c_str());
+  try {
+    parser.ParseQuery("SELECT * F_ROM test_table;");
+    BUSTUB_ASSERT(false, "Shouldn't reach here.");
+  } catch (std::exception &exn) {
+    LOG_INFO("Caught parse-failure exception.");
   }
 }
 
