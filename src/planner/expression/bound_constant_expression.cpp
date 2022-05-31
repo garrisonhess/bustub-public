3df2302cbe825f315bfed8678963747f7e18c4da
Garrison Hess <garrison.hess@gmail.com>
2022-05-30 23:49:40 -0400

    Compiling and linking planner

--- src/planner/expression/bound_constant_expression.cpp
+++ src/planner/expression/bound_constant_expression.cpp
@@ -1,6 +1,7 @@
 #include "planner/expression/bound_constant_expression.h"
 
 // #include "common/value_operations/value_operations.h"
+#include "common/exception.h"
 #include "common/hash_util.h"
 
 namespace bustub {
@@ -9,25 +10,30 @@ BoundConstantExpression::BoundConstantExpression(Value value_p)
     : Expression(ExpressionType::VALUE_CONSTANT, ExpressionClass::BOUND_CONSTANT, Type(value_p.GetTypeId())),
       value(value_p) {}
 
-// string BoundConstantExpression::ToString() const { return value.ToSQLString(); }
+string BoundConstantExpression::ToString() const {
+  throw NotImplementedException("");
+  // return value.ToSQLString();
+}
 
-// bool BoundConstantExpression::Equals(const BaseExpression *other_p) const {
-//   if (!Expression::Equals(other_p)) {
-//     return false;
-//   }
-//   auto other = (BoundConstantExpression *)other_p;
-//   return !ValueOperations::DistinctFrom(value, other->value);
-// }
+bool BoundConstantExpression::Equals(const BaseExpression *other_p) const {
+  if (!Expression::Equals(other_p)) {
+    return false;
+  }
+  return false;
+  // auto other = (BoundConstantExpression *)other_p;
+  // return !ValueOperations::DistinctFrom(value, other->value);
+}
 
-// hash_t BoundConstantExpression::Hash() const {
-//   hash_t result = Expression::Hash();
-//   return CombineHash(value.Hash(), result);
-// }
+hash_t BoundConstantExpression::Hash() const {
+  throw NotImplementedException("");
+  // hash_t result = Expression::Hash();
+  // return CombineHash(value.Hash(), result);
+}
 
-// unique_ptr<Expression> BoundConstantExpression::Copy() {
-//   auto copy = make_unique<BoundConstantExpression>(value);
-//   copy->CopyProperties(*this);
-//   return move(copy);
-// }
+unique_ptr<Expression> BoundConstantExpression::Copy() {
+  auto copy = make_unique<BoundConstantExpression>(value);
+  copy->CopyProperties(*this);
+  return copy;
+}
 
 }  // namespace bustub
--- src/planner/table_binding.cpp
+++ src/planner/table_binding.cpp
@@ -24,49 +24,52 @@ Binding::Binding(const string &alias, vector<Type> coltypes, vector<string> coln
   }
 }
 
-// bool Binding::TryGetBindingIndex(const string &column_name, uint64_t &result) {
-//   throw bustub::NotImplementedException("");
-//   //   auto entry = name_map.find(column_name);
-//   //   if (entry != name_map.end()) {
-//   //     result = entry->second;
-//   //     return true;
-//   //   }
-//   //   return false;
-// }
+bool Binding::TryGetBindingIndex(const string &column_name, uint64_t &result) {
+  throw bustub::NotImplementedException("");
+  //   auto entry = name_map.find(column_name);
+  //   if (entry != name_map.end()) {
+  //     result = entry->second;
+  //     return true;
+  //   }
+  //   return false;
+}
 
-// uint64_t Binding::GetBindingIndex(const string &column_name) {
-//   uint64_t result;
-//   if (!TryGetBindingIndex(column_name, result)) {
-//     throw Exception("Binding index for column not found");
-//   }
-//   return result;
-// }
+uint64_t Binding::GetBindingIndex(const string &column_name) {
+  throw bustub::NotImplementedException("");
+  // uint64_t result;
+  // if (!TryGetBindingIndex(column_name, result)) {
+  //   throw Exception("Binding index for column not found");
+  // }
+  // return result;
+}
 
-// bool Binding::HasMatchingBinding(const string &column_name) {
-//   uint64_t result;
-//   return TryGetBindingIndex(column_name, result);
-// }
+bool Binding::HasMatchingBinding(const string &column_name) {
+  throw bustub::NotImplementedException("");
+  // uint64_t result;
+  // return TryGetBindingIndex(column_name, result);
+}
 
-// string Binding::ColumnNotFoundError(const string &column_name) const {
-//   return StringUtil::Format("Values list  does not have a column named ");
-// }
+string Binding::ColumnNotFoundError(const string &column_name) const {
+  return StringUtil::Format("Values list  does not have a column named ");
+}
 
-// BindResult Binding::Bind(ColumnRefExpression &colref, uint64_t depth) {
-//   uint64_t column_index;
-//   if (!TryGetBindingIndex(colref.GetColumnName(), column_index)) {
-//     return BindResult(ColumnNotFoundError(colref.GetColumnName()));
-//   }
-//   ColumnBinding binding;
-//   binding.table_index = index;
-//   binding.column_index = column_index;
-//   Type sql_type = types[column_index];
-//   if (colref.alias_.empty()) {
-//     colref.alias_ = names[column_index];
-//   }
-//   return BindResult(make_unique<BoundColumnRefExpression>(colref.GetName(), sql_type, binding, depth));
-// }
+BindResult Binding::Bind(ColumnRefExpression &colref, uint64_t depth) {
+  throw bustub::NotImplementedException("");
+  // uint64_t column_index;
+  // if (!TryGetBindingIndex(colref.GetColumnName(), column_index)) {
+  //   return BindResult(ColumnNotFoundError(colref.GetColumnName()));
+  // }
+  // ColumnBinding binding;
+  // binding.table_index = index;
+  // binding.column_index = column_index;
+  // Type sql_type = types[column_index];
+  // if (colref.alias_.empty()) {
+  //   colref.alias_ = names[column_index];
+  // }
+  // return BindResult(make_unique<BoundColumnRefExpression>(colref.GetName(), sql_type, binding, depth));
+}
 
-// TableCatalogEntry *Binding::GetTableEntry() { return nullptr; }
+TableCatalogEntry *Binding::GetTableEntry() { return nullptr; }
 
 TableBinding::TableBinding(const string &alias, vector<Type> types_p, vector<string> names_p, LogicalGet &get,
                            uint64_t index, bool add_row_id)
@@ -78,74 +81,53 @@ TableBinding::TableBinding(const string &alias, vector<Type> types_p, vector<str
   }
 }
 
-// BindResult TableBinding::Bind(ColumnRefExpression &colref, uint64_t depth) {
-//   auto &column_name = colref.GetColumnName();
-//   uint64_t column_index;
-//   if (!TryGetBindingIndex(column_name, column_index)) {
-//     return BindResult(ColumnNotFoundError(column_name));
-//   }
-//   // fetch the type of the column
-//   Type col_type;
-//   if (column_index == COLUMN_IDENTIFIER_ROW_ID) {
-//     // row id: BIGINT type
-//     col_type = Type(TypeId::BIGINT);
-//   } else {
-//     // normal column: fetch type from base column
-//     col_type = types[column_index];
-//     if (colref.alias_.empty()) {
-//       colref.alias_ = names[column_index];
-//     }
-//   }
+BindResult TableBinding::Bind(ColumnRefExpression &colref, uint64_t depth) {
+  throw bustub::NotImplementedException("");
 
-//   auto &column_ids = get.column_ids;
-//   // check if the entry already exists in the column list for the table
-//   ColumnBinding binding;
+  // auto &column_name = colref.GetColumnName();
+  // uint64_t column_index;
+  // if (!TryGetBindingIndex(column_name, column_index)) {
+  //   return BindResult(ColumnNotFoundError(column_name));
+  // }
+  // // fetch the type of the column
+  // Type col_type;
+  // if (column_index == COLUMN_IDENTIFIER_ROW_ID) {
+  //   // row id: BIGINT type
+  //   col_type = Type(TypeId::BIGINT);
+  // } else {
+  //   // normal column: fetch type from base column
+  //   col_type = types[column_index];
+  //   if (colref.alias_.empty()) {
+  //     colref.alias_ = names[column_index];
+  //   }
+  // }
 
-//   binding.column_index = column_ids.size();
-//   for (uint64_t i = 0; i < column_ids.size(); i++) {
-//     if (column_ids[i] == column_index) {
-//       binding.column_index = i;
-//       break;
-//     }
-//   }
-//   if (binding.column_index == column_ids.size()) {
-//     // column binding not found: add it to the list of bindings
-//     column_ids.push_back(column_index);
-//   }
-//   binding.table_index = index;
-//   return BindResult(make_unique<BoundColumnRefExpression>(colref.GetName(), col_type, binding, depth));
-// }
+  // auto &column_ids = get.column_ids;
+  // // check if the entry already exists in the column list for the table
+  // ColumnBinding binding;
 
-// TableCatalogEntry *TableBinding::GetTableEntry() { return get.GetTable(); }
+  // binding.column_index = column_ids.size();
+  // for (uint64_t i = 0; i < column_ids.size(); i++) {
+  //   if (column_ids[i] == column_index) {
+  //     binding.column_index = i;
+  //     break;
+  //   }
+  // }
+  // if (binding.column_index == column_ids.size()) {
+  //   // column binding not found: add it to the list of bindings
+  //   column_ids.push_back(column_index);
+  // }
+  // binding.table_index = index;
+  // return BindResult(make_unique<BoundColumnRefExpression>(colref.GetName(), col_type, binding, depth));
+}
 
-// string TableBinding::ColumnNotFoundError(const string &column_name) const {
-//   return StringUtil::Format("Table does not have a column named");
-// }
+TableCatalogEntry *TableBinding::GetTableEntry() {
+  throw bustub::NotImplementedException("");
+  // return get.GetTable();
+}
 
-// MacroBinding::MacroBinding(vector<Type> types_p, vector<string> names_p, string macro_name_p)
-//     : Binding(MacroBinding::MACRO_NAME, move(types_p), move(names_p), -1), macro_name(move(macro_name_p)) {}
-
-// BindResult MacroBinding::Bind(ColumnRefExpression &colref, uint64_t depth) {
-//   uint64_t column_index;
-//   if (!TryGetBindingIndex(colref.GetColumnName(), column_index)) {
-//     throw Exception("Column not found in macro");
-//   }
-//   ColumnBinding binding;
-//   binding.table_index = index;
-//   binding.column_index = column_index;
-
-//   // we are binding a parameter to create the macro, no arguments are supplied
-//   return BindResult(make_unique<BoundColumnRefExpression>(colref.GetName(), types[column_index], binding, depth));
-// }
-
-// unique_ptr<ParsedExpression> MacroBinding::ParamToArg(ColumnRefExpression &colref) {
-//   uint64_t column_index;
-//   if (!TryGetBindingIndex(colref.GetColumnName(), column_index)) {
-//     throw Exception("Column not found in macro");
-//   }
-//   auto arg = arguments[column_index]->Copy();
-//   arg->alias_ = colref.alias_;
-//   return arg;
-// }
+string TableBinding::ColumnNotFoundError(const string &column_name) const {
+  return StringUtil::Format("Table does not have a column named");
+}
 
 }  // namespace bustub
