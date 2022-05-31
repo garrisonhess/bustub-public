30581ace543add6d9f39d62a997371f4c9f7b29a
Garrison Hess <garrison.hess@gmail.com>
2022-05-31 18:49:41 -0400

    Checkpoint

--- src/include/execution/physical_plan_generator.h
+++ src/include/execution/physical_plan_generator.h
@@ -9,49 +9,35 @@
 #pragma once
 
 #include "common/common.h"
-#include "execution/physical_operator.h"
+#include "execution/plans/abstract_plan.h"
 #include "planner/logical_operator.h"
 #include "planner/logical_tokens.h"
 
-namespace duckdb {
+namespace bustub {
 class ClientContext;
 
 //! The physical plan generator generates a physical execution plan from a
 //! logical query plan
 class PhysicalPlanGenerator {
  public:
-  explicit PhysicalPlanGenerator(ClientContext &context) : context(context) {}
-
-  unordered_set<CatalogEntry *> dependencies;
+  explicit PhysicalPlanGenerator(ClientContext &context) : context_(context) {}
 
  public:
   //! Creates a plan from the logical operator. This involves resolving column bindings and generating physical
   //! operator nodes.
-  unique_ptr<PhysicalOperator> CreatePlan(unique_ptr<LogicalOperator> logical);
+  unique_ptr<AbstractPlanNode> CreatePlan(unique_ptr<LogicalOperator> op);
 
  protected:
-  unique_ptr<PhysicalOperator> CreatePlan(LogicalOperator &op);
+  unique_ptr<AbstractPlanNode> CreatePlan(LogicalOperator &op);
 
-  unique_ptr<PhysicalOperator> CreatePlan(LogicalCreateTable &op);
-  unique_ptr<PhysicalOperator> CreatePlan(LogicalCreateIndex &op);
-  unique_ptr<PhysicalOperator> CreatePlan(LogicalDelete &op);
-  unique_ptr<PhysicalOperator> CreatePlan(LogicalDistinct &op);
-  unique_ptr<PhysicalOperator> CreatePlan(LogicalDummyScan &expr);
-  unique_ptr<PhysicalOperator> CreatePlan(LogicalEmptyResult &op);
-  unique_ptr<PhysicalOperator> CreatePlan(LogicalExport &op);
-  unique_ptr<PhysicalOperator> CreatePlan(LogicalFilter &op);
-  unique_ptr<PhysicalOperator> CreatePlan(LogicalGet &op);
-  unique_ptr<PhysicalOperator> CreatePlan(LogicalLimit &op);
-  unique_ptr<PhysicalOperator> CreatePlan(LogicalLimitPercent &op);
-  unique_ptr<PhysicalOperator> CreatePlan(LogicalOrder &op);
-  unique_ptr<PhysicalOperator> CreatePlan(LogicalProjection &op);
-  unique_ptr<PhysicalOperator> CreatePlan(LogicalInsert &op);
-  unique_ptr<PhysicalOperator> CreatePlan(LogicalUpdate &op);
-
-  unique_ptr<PhysicalOperator> CreateDistinctOn(unique_ptr<PhysicalOperator> child,
-                                                vector<unique_ptr<Expression>> distinct_targets);
+  unique_ptr<AbstractPlanNode> CreatePlan(LogicalCreateTable &op);
+  unique_ptr<AbstractPlanNode> CreatePlan(LogicalCreateIndex &op);
+  unique_ptr<AbstractPlanNode> CreatePlan(LogicalDelete &op);
+  unique_ptr<AbstractPlanNode> CreatePlan(LogicalDistinct &op);
+  unique_ptr<AbstractPlanNode> CreatePlan(LogicalDummyScan &expr);
+  unique_ptr<AbstractPlanNode> CreatePlan(LogicalEmptyResult &op);
 
  private:
-  ClientContext &context;
+  ClientContext &context_;
 };
-}  // namespace duckdb
+}  // namespace bustub
