1b7382584752138be3af7f9cdd26b83d0bd95041
Garrison Hess <garrison.hess@gmail.com>
2022-06-02 18:53:01 -0400

    logical join

--- src/include/planner/operator/logical_join.h
+++ src/include/planner/operator/logical_join.h
@@ -1,42 +1,42 @@
-// //===----------------------------------------------------------------------===//
-// //                         BusTub
-// //
-// // planner/operator/logical_join.h
-// //
-// //
-// //===----------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
+//                         BusTub
+//
+// planner/operator/logical_join.h
+//
+//
+//===----------------------------------------------------------------------===//
 
-// #pragma once
+#pragma once
 
-// #include <unordered_set>
-// #include "common/enums/join_type.h"
-// #include "planner/logical_operator.h"
+#include <unordered_set>
+#include "common/enums/join_type.h"
+#include "planner/logical_operator.h"
 
-// namespace bustub {
+namespace bustub {
 
-// //! LogicalJoin represents a join between two relations
-// class LogicalJoin : public LogicalOperator {
-//  public:
-//   explicit LogicalJoin(JoinType type, LogicalOperatorType logical_type = LogicalOperatorType::LOGICAL_JOIN);
+//! LogicalJoin represents a join between two relations
+class LogicalJoin : public LogicalOperator {
+ public:
+  explicit LogicalJoin(JoinType type, LogicalOperatorType logical_type = LogicalOperatorType::LOGICAL_JOIN);
 
-//   // Gets the set of table references that are reachable from this node
-//   static void GetTableReferences(LogicalOperator &op, std::unordered_set<uint64_t> &bindings);
-//   static void GetExpressionBindings(Expression &expr, std::unordered_set<uint64_t> &bindings);
+  // Gets the set of table references that are reachable from this node
+  static void GetTableReferences(LogicalOperator &op, std::unordered_set<uint64_t> &bindings);
+  static void GetExpressionBindings(Expression &expr, std::unordered_set<uint64_t> &bindings);
 
-//   //! The type of the join (INNER, OUTER, etc...)
-//   JoinType join_type;
-//   //! Table index used to refer to the MARK column (in case of a MARK join)
-//   uint64_t mark_index;
-//   //! The columns of the LHS that are output by the join
-//   vector<uint64_t> left_projection_map;
-//   //! The columns of the RHS that are output by the join
-//   vector<uint64_t> right_projection_map;
+  //! The type of the join (INNER, OUTER, etc...)
+  JoinType join_type_;
+  //! Table index used to refer to the MARK column (in case of a MARK join)
+  uint64_t mark_index_;
+  //! The columns of the LHS that are output by the join
+  vector<uint64_t> left_projection_map_;
+  //! The columns of the RHS that are output by the join
+  vector<uint64_t> right_projection_map_;
 
-//  public:
-//   vector<ColumnBinding> GetColumnBindings() override;
+ public:
+  vector<ColumnBinding> GetColumnBindings() override;
 
-//  protected:
-//   void ResolveTypes() override;
-// };
+ protected:
+  void ResolveTypes() override;
+};
 
-// }  // namespace bustub
+}  // namespace bustub
--- src/planner/operator/logical_join.cpp
+++ src/planner/operator/logical_join.cpp
@@ -1,42 +1,42 @@
-// #include "planner/operator/logical_join.h"
+#include "planner/operator/logical_join.h"
 
-// #include "planner/expression/bound_columnref_expression.h"
-// #include "planner/expression_iterator.h"
+#include "planner/expression/bound_columnref_expression.h"
+#include "planner/expression_iterator.h"
 
-// namespace bustub {
+namespace bustub {
 
-// LogicalJoin::LogicalJoin(JoinType join_type, LogicalOperatorType logical_type)
-//     : LogicalOperator(logical_type), join_type(join_type) {}
+LogicalJoin::LogicalJoin(JoinType join_type, LogicalOperatorType logical_type)
+    : LogicalOperator(logical_type), join_type_(join_type) {}
 
-// vector<ColumnBinding> LogicalJoin::GetColumnBindings() {
-//   auto left_bindings = MapBindings(children[0]->GetColumnBindings(), left_projection_map);
-//   // for other join types we project both the LHS and the RHS
-//   auto right_bindings = MapBindings(children[1]->GetColumnBindings(), right_projection_map);
-//   left_bindings.insert(left_bindings.end(), right_bindings.begin(), right_bindings.end());
-//   return left_bindings;
-// }
+vector<ColumnBinding> LogicalJoin::GetColumnBindings() {
+  auto left_bindings = MapBindings(children_[0]->GetColumnBindings(), left_projection_map_);
+  // for other join types we project both the LHS and the RHS
+  auto right_bindings = MapBindings(children_[1]->GetColumnBindings(), right_projection_map_);
+  left_bindings.insert(left_bindings.end(), right_bindings.begin(), right_bindings.end());
+  return left_bindings;
+}
 
-// void LogicalJoin::ResolveTypes() {
-//   types = MapTypes(children[0]->types, left_projection_map);
-//   // for any other join we project both sides
-//   auto right_types = MapTypes(children[1]->types, right_projection_map);
-//   types.insert(types.end(), right_types.begin(), right_types.end());
-// }
+void LogicalJoin::ResolveTypes() {
+  types_ = MapTypes(children_[0]->types_, left_projection_map_);
+  // for any other join we project both sides
+  auto right_types = MapTypes(children_[1]->types_, right_projection_map_);
+  types_.insert(types_.end(), right_types.begin(), right_types.end());
+}
 
-// void LogicalJoin::GetTableReferences(LogicalOperator &op, unordered_set<uint64_t> &bindings) {
-//   auto column_bindings = op.GetColumnBindings();
-//   for (auto binding : column_bindings) {
-//     bindings.insert(binding.table_index);
-//   }
-// }
+void LogicalJoin::GetTableReferences(LogicalOperator &op, unordered_set<uint64_t> &bindings) {
+  auto column_bindings = op.GetColumnBindings();
+  for (auto binding : column_bindings) {
+    bindings.insert(binding.table_index_);
+  }
+}
 
-// void LogicalJoin::GetExpressionBindings(Expression &expr, unordered_set<uint64_t> &bindings) {
-//   if (expr.type == ExpressionType::BOUND_COLUMN_REF) {
-//     auto &colref = (BoundColumnRefExpression &)expr;
-//     assert(colref.depth == 0);
-//     bindings.insert(colref.binding.table_index);
-//   }
-//   ExpressionIterator::EnumerateChildren(expr, [&](Expression &child) { GetExpressionBindings(child, bindings); });
-// }
+void LogicalJoin::GetExpressionBindings(Expression &expr, unordered_set<uint64_t> &bindings) {
+  if (expr.type_ == ExpressionType::BOUND_COLUMN_REF) {
+    auto &colref = (BoundColumnRefExpression &)expr;
+    assert(colref.depth_ == 0);
+    bindings.insert(colref.binding_.table_index_);
+  }
+  ExpressionIterator::EnumerateChildren(expr, [&](Expression &child) { GetExpressionBindings(child, bindings); });
+}
 
-// }  // namespace bustub
+}  // namespace bustub
