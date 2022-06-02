13d8cff62d91196f611cbd5a06682f1eefd7bf2f
Garrison Hess <garrison.hess@gmail.com>
2022-06-02 18:45:56 -0400

    select node bindings

--- src/planner/expression_binder/order_binder.cpp
+++ src/planner/expression_binder/order_binder.cpp
@@ -1,119 +1,108 @@
-// #include "planner/expression_binder/order_binder.h"
+#include "planner/expression_binder/order_binder.h"
 
-// #include "parser/expression/columnref_expression.h"
-// #include "parser/expression/constant_expression.h"
-// #include "parser/expression/star_expression.h"
-// #include "parser/query_node/select_node.h"
-// #include "planner/expression_binder.h"
+#include "common/exception.h"
+#include "parser/expression/columnref_expression.h"
+#include "parser/expression/constant_expression.h"
+#include "parser/expression/star_expression.h"
+#include "parser/query_node/select_node.h"
+#include "planner/expression_binder.h"
 
-// namespace bustub {
+namespace bustub {
 
-// OrderBinder::OrderBinder(vector<Binder *> binders, uint64_t projection_index,
-//                          case_insensitive_map_t<uint64_t> &alias_map, expression_map_t<uint64_t> &projection_map,
-//                          uint64_t max_count)
-//     : binders(move(binders)),
-//       projection_index(projection_index),
-//       max_count(max_count),
-//       extra_list(nullptr),
-//       alias_map(alias_map),
-//       projection_map(projection_map) {}
-// OrderBinder::OrderBinder(vector<Binder *> binders, uint64_t projection_index, SelectNode &node,
-//                          case_insensitive_map_t<uint64_t> &alias_map, expression_map_t<uint64_t> &projection_map)
-//     : binders(move(binders)), projection_index(projection_index), alias_map(alias_map),
-//     projection_map(projection_map) {
-//   this->max_count = node.select_list.size();
-//   this->extra_list = &node.select_list;
-// }
+OrderBinder::OrderBinder(vector<Binder *> binders, uint64_t projection_index,
+                         case_insensitive_map_t<uint64_t> &alias_map, expression_map_t<uint64_t> &projection_map,
+                         uint64_t max_count)
+    : binders_(move(binders)),
+      projection_index_(projection_index),
+      max_count_(max_count),
+      extra_list_(nullptr),
+      alias_map_(alias_map),
+      projection_map_(projection_map) {}
 
-// unique_ptr<Expression> OrderBinder::CreateProjectionReference(ParsedExpression &expr, uint64_t index) {
-//   string alias;
-//   if (extra_list && index < extra_list->size()) {
-//     alias = extra_list->at(index)->ToString();
-//   } else {
-//     alias = expr.GetName();
-//   }
-//   return make_unique<BoundColumnRefExpression>(move(alias), Type::INVALID, ColumnBinding(projection_index, index));
-// }
+OrderBinder::OrderBinder(vector<Binder *> binders, uint64_t projection_index, SelectNode &node,
+                         case_insensitive_map_t<uint64_t> &alias_map, expression_map_t<uint64_t> &projection_map)
+    : binders_(move(binders)),
+      projection_index_(projection_index),
+      alias_map_(alias_map),
+      projection_map_(projection_map) {
+  this->max_count_ = node.select_list_.size();
+  this->extra_list_ = &node.select_list_;
+}
 
-// unique_ptr<Expression> OrderBinder::CreateExtraReference(unique_ptr<ParsedExpression> expr) {
-//   auto result = CreateProjectionReference(*expr, extra_list->size());
-//   extra_list->push_back(move(expr));
-//   return result;
-// }
+unique_ptr<Expression> OrderBinder::CreateProjectionReference(ParsedExpression &expr, uint64_t index) {
+  string alias;
+  if ((extra_list_ != nullptr) && index < extra_list_->size()) {
+    alias = extra_list_->at(index)->ToString();
+  } else {
+    alias = expr.GetName();
+  }
+  return make_unique<BoundColumnRefExpression>(move(alias), Type(TypeId::INVALID),
+                                               ColumnBinding(projection_index_, index));
+}
 
-// unique_ptr<Expression> OrderBinder::Bind(unique_ptr<ParsedExpression> expr) {
-//   // in the ORDER BY clause we do not bind children
-//   // we bind ONLY to the select list
-//   // if there is no matching entry in the SELECT list already, we add the expression to the SELECT list and refer the
-//   // new expression the new entry will then be bound later during the binding of the SELECT list we also don't do
-//   type
-//   // resolution here: this only happens after the SELECT list has been bound
-//   switch (expr->expression_class) {
-//     case ExpressionClass::CONSTANT: {
-//       // ORDER BY constant
-//       // is the ORDER BY expression a constant integer? (e.g. ORDER BY 1)
-//       auto &constant = (ConstantExpression &)*expr;
-//       // ORDER BY a constant
-//       if (!constant.value.type().IsIntegral()) {
-//         // non-integral expression, we just leave the constant here.
-//         // ORDER BY <constant> has no effect
-//         // CONTROVERSIAL: maybe we should throw an error
-//         return nullptr;
-//       }
-//       // INTEGER constant: we use the integer as an index into the select list (e.g. ORDER BY 1)
-//       auto index = (uint64_t)constant.value.GetValue<int64_t>();
-//       if (index < 1 || index > max_count) {
-//         throw Exception("ORDER term out of range - should be between 1 and %lld", (uint64_t)max_count);
-//       }
-//       return CreateProjectionReference(*expr, index - 1);
-//     }
-//     case ExpressionClass::COLUMN_REF: {
-//       // COLUMN REF expression
-//       // check if we can bind it to an alias in the select list
-//       auto &colref = (ColumnRefExpression &)*expr;
-//       // if there is an explicit table name we can't bind to an alias
-//       if (colref.IsQualified()) {
-//         break;
-//       }
-//       // check the alias list
-//       auto entry = alias_map.find(colref.column_names[0]);
-//       if (entry != alias_map.end()) {
-//         // it does! point it to that entry
-//         return CreateProjectionReference(*expr, entry->second);
-//       }
-//       break;
-//     }
-//     case ExpressionClass::POSITIONAL_REFERENCE: {
-//       auto &posref = (PositionalReferenceExpression &)*expr;
-//       return CreateProjectionReference(*expr, posref.index - 1);
-//     }
-//     default:
-//       break;
-//   }
-//   // general case
-//   // first bind the table names of this entry
-//   for (auto &binder : binders) {
-//     ExpressionBinder::QualifyColumnNames(*binder, expr);
-//   }
-//   // first check if the ORDER BY clause already points to an entry in the projection list
-//   auto entry = projection_map.find(expr.get());
-//   if (entry != projection_map.end()) {
-//     if (entry->second == INVALID_INDEX) {
-//       throw Exception("Ambiguous reference to column");
-//     }
-//     // there is a matching entry in the projection list
-//     // just point to that entry
-//     return CreateProjectionReference(*expr, entry->second);
-//   }
-//   if (!extra_list) {
-//     // no extra list specified: we cannot push an extra ORDER BY clause
-//     throw Exception(
-//         "Could not ORDER BY column \"%s\": add the expression/function to every SELECT, or move "
-//         "the UNION into a FROM clause.",
-//         expr->ToString());
-//   }
-//   // otherwise we need to push the ORDER BY entry into the select list
-//   return CreateExtraReference(move(expr));
-// }
+unique_ptr<Expression> OrderBinder::CreateExtraReference(unique_ptr<ParsedExpression> expr) {
+  auto result = CreateProjectionReference(*expr, extra_list_->size());
+  extra_list_->push_back(move(expr));
+  return result;
+}
 
-// }  // namespace bustub
+unique_ptr<Expression> OrderBinder::Bind(unique_ptr<ParsedExpression> expr) {
+  // in the ORDER BY clause we do not bind children
+  // we bind ONLY to the select list
+  // if there is no matching entry in the SELECT list already, we add the expression to the SELECT list and refer the
+  // new expression the new entry will then be bound later during the binding of the SELECT list we also don't do
+  // type resolution here: this only happens after the SELECT list has been bound
+  switch (expr->expression_class_) {
+    case ExpressionClass::CONSTANT:
+      // Note(GH): Just returning null when order by constant, instead of using constant as a column index.
+      return nullptr;
+    case ExpressionClass::COLUMN_REF: {
+      // COLUMN REF expression
+      // check if we can bind it to an alias in the select list
+      auto &colref = (ColumnRefExpression &)*expr;
+      // if there is an explicit table name we can't bind to an alias
+      if (colref.IsQualified()) {
+        break;
+      }
+      // check the alias list
+      auto entry = alias_map_.find(colref.column_names_[0]);
+      if (entry != alias_map_.end()) {
+        // it does! point it to that entry
+        return CreateProjectionReference(*expr, entry->second);
+      }
+      break;
+    }
+    case ExpressionClass::POSITIONAL_REFERENCE: {
+      throw NotImplementedException("No positional ref ordering yet.");
+      //   auto &posref = (PositionalReferenceExpression &)*expr;
+      //   return CreateProjectionReference(*expr, posref.index - 1);
+    }
+    default:
+      break;
+  }
+  // general case
+  // first bind the table names of this entry
+  for (auto &binder : binders_) {
+    ExpressionBinder::QualifyColumnNames(*binder, expr);
+  }
+  // first check if the ORDER BY clause already points to an entry in the projection list
+  auto entry = projection_map_.find(expr.get());
+  if (entry != projection_map_.end()) {
+    if (entry->second == INVALID_INDEX) {
+      throw Exception("Ambiguous reference to column");
+    }
+    // there is a matching entry in the projection list
+    // just point to that entry
+    return CreateProjectionReference(*expr, entry->second);
+  }
+  if (extra_list_ == nullptr) {
+    // no extra list specified: we cannot push an extra ORDER BY clause
+    throw Exception(
+        "Could not ORDER BY column X: add the expression/function to every SELECT, or move "
+        "the UNION into a FROM clause.");
+  }
+  // otherwise we need to push the ORDER BY entry into the select list
+  return CreateExtraReference(move(expr));
+}
+
+}  // namespace bustub
--- src/planner/expression_binder/select_binder.cpp
+++ src/planner/expression_binder/select_binder.cpp
@@ -15,11 +15,11 @@ SelectBinder::SelectBinder(Binder &binder, ClientContext &context, BoundSelectNo
 
 BindResult SelectBinder::BindExpression(unique_ptr<ParsedExpression> *expr_ptr, uint64_t depth, bool root_expression) {
   auto &expr = **expr_ptr;
-  //   // check if the expression binds to one of the groups
-  //   auto group_index = TryBindGroup(expr, depth);
-  //   if (group_index != INVALID_INDEX) {
-  //     return BindGroup(expr, depth, group_index);
-  //   }
+    // check if the expression binds to one of the groups
+    auto group_index = TryBindGroup(expr, depth);
+    if (group_index != INVALID_INDEX) {
+      return BindGroup(expr, depth, group_index);
+    }
   switch (expr.expression_class_) {
     case ExpressionClass::DEFAULT:
       return BindResult("SELECT clause cannot contain DEFAULT clause");
@@ -30,37 +30,36 @@ BindResult SelectBinder::BindExpression(unique_ptr<ParsedExpression> *expr_ptr, 
   }
 }
 
-// uint64_t SelectBinder::TryBindGroup(ParsedExpression &expr, uint64_t depth) {
-//   // first check the group alias map, if expr is a ColumnRefExpression
-//   if (expr.type == ExpressionType::COLUMN_REF) {
-//     auto &colref = (ColumnRefExpression &)expr;
-//     if (!colref.IsQualified()) {
-//       auto alias_entry = info.alias_map.find(colref.column_names[0]);
-//       if (alias_entry != info.alias_map.end()) {
-//         // found entry!
-//         return alias_entry->second;
-//       }
-//     }
-//   }
-//   // no alias reference found
-//   // check the list of group columns for a match
-//   auto entry = info.map.find(&expr);
-//   if (entry != info.map.end()) {
-//     return entry->second;
-//   }
+uint64_t SelectBinder::TryBindGroup(ParsedExpression &expr, uint64_t depth) {
+  // first check the group alias map, if expr is a ColumnRefExpression
+  if (expr.type_ == ExpressionType::COLUMN_REF) {
+    auto &colref = (ColumnRefExpression &)expr;
+    if (!colref.IsQualified()) {
+      auto alias_entry = info_.alias_map_.find(colref.column_names_[0]);
+      if (alias_entry != info_.alias_map_.end()) {
+        // found entry!
+        return alias_entry->second;
+      }
+    }
+  }
+  // no alias reference found
+  // check the list of group columns for a match
+  auto entry = info_.map_.find(&expr);
+  if (entry != info_.map_.end()) {
+    return entry->second;
+  }
 // #ifdef DEBUG
-//   for (auto entry : info.map) {
-//     assert(!entry.first->Equals(&expr));
-//     assert(!expr.Equals(entry.first));
-//   }
+  for (auto entry : info_.map_) {
+    assert(!entry.first->Equals(&expr));
+    assert(!expr.Equals(entry.first));
+  }
 // #endif
-//   return INVALID_INDEX;
-// }
+  return INVALID_INDEX;
+}
 
-// BindResult SelectBinder::BindGroup(ParsedExpression &expr, uint64_t depth, uint64_t group_index) {
-//   auto &group = node.groups.group_expressions[group_index];
-//   return BindResult(make_unique<BoundColumnRefExpression>(expr.GetName(), group->return_type,
-//                                                           ColumnBinding(node.group_index, group_index), depth));
-// }
+BindResult SelectBinder::BindGroup(ParsedExpression &expr, uint64_t depth, uint64_t group_index) {
+  auto &group = node_.groups_.group_expressions_[group_index];
+  return BindResult(make_unique<BoundColumnRefExpression>(expr.GetName(), group->return_type_, ColumnBinding(node_.group_index_, group_index), depth));
+}
 
 }  // namespace bustub
