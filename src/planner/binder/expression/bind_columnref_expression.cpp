54119ea31041586c839dc6e75390d5081b90b068
Garrison Hess <garrison.hess@gmail.com>
2022-06-02 11:40:53 -0400

    Checkpoint

--- src/planner/binder/expression/bind_columnref_expression.cpp
+++ src/planner/binder/expression/bind_columnref_expression.cpp
@@ -19,29 +19,31 @@ BindResult ExpressionBinder::BindExpression(ColumnRefExpression &colref_p, uint6
     return BindResult(make_unique<BoundConstantExpression>(val));
   }
 
-  string error_message;
-  auto expr = QualifyColumnName(colref_p, error_message);
-  if (!expr) {
-    return BindResult(binder_.FormatError(colref_p, error_message));
-  }
+  // string error_message;
+  // auto expr = QualifyColumnName(colref_p, error_message);
+  // if (!expr) {
+  //   return BindResult(binder_.FormatError(colref_p, error_message));
+  // }
   // if (expr->type_ != ExpressionType::COLUMN_REF) {
   //   return BindExpression(&expr, depth);
   // }
-  auto &colref = (ColumnRefExpression &)*expr;
-  assert(colref.column_names_.size() == 2 || colref.column_names_.size() == 3);
-  // auto &table_name = colref.column_names_.size() == 3 ? colref.column_names_[1] : colref.column_names_[0];
-  // individual column reference
-  // resolve to either a base table or a subquery expression
-  BindResult result = binder_.bind_context_.BindColumn(colref, depth);
-  if (!result.HasError()) {
-    BoundColumnReferenceInfo ref;
-    ref.name_ = colref.column_names_.back();
-    ref.query_location_ = colref.query_location_;
-    bound_columns_.push_back(move(ref));
-  } else {
-    result.error_ = binder_.FormatError(colref_p, result.error_);
-  }
-  return result;
+  // auto &colref = (ColumnRefExpression &)*expr;
+  // assert(colref.column_names_.size() == 2 || colref.column_names_.size() == 3);
+  // // auto &table_name = colref.column_names_.size() == 3 ? colref.column_names_[1] : colref.column_names_[0];
+  // // individual column reference
+  // // resolve to either a base table or a subquery expression
+  // BindResult result = binder_.bind_context_.BindColumn(colref, depth);
+  // if (!result.HasError()) {
+  //   BoundColumnReferenceInfo ref;
+  //   ref.name_ = colref.column_names_.back();
+  //   ref.query_location_ = colref.query_location_;
+  //   bound_columns_.push_back(move(ref));
+  // } else {
+  //   result.error_ = binder_.FormatError(colref_p, result.error_);
+  // }
+  BindResult r;
+  return r;
+  // return result;
 }
 
 }  // namespace bustub
