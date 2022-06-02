297f569ecf3cd0047af5b8a7e58f4f914b021e19
Garrison Hess <garrison.hess@gmail.com>
2022-06-02 18:51:02 -0400

    logical limit

--- src/include/planner/operator/logical_limit.h
+++ src/include/planner/operator/logical_limit.h
@@ -1,36 +1,34 @@
-// //===----------------------------------------------------------------------===//
-// //                         BusTub
-// //
-// // planner/operator/logical_limit.h
-// //
-// //
-// //===----------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
+//                         BusTub
+//
+// planner/operator/logical_limit.h
+//
+//
+//===----------------------------------------------------------------------===//
 
-// #pragma once
+#pragma once
 
-// #include "planner/logical_operator.h"
+#include "planner/logical_operator.h"
 
-// namespace bustub {
+namespace bustub {
 
-// //! LogicalLimit represents a LIMIT clause
-// class LogicalLimit : public LogicalOperator {
-//  public:
-//   LogicalLimit(int64_t limit_val, int64_t offset_val, unique_ptr<Expression> limit, unique_ptr<Expression> offset);
+//! LogicalLimit represents a LIMIT clause
+class LogicalLimit : public LogicalOperator {
+ public:
+  LogicalLimit(int64_t limit_val, int64_t offset_val, unique_ptr<Expression> limit, unique_ptr<Expression> offset);
 
-//   //! Limit and offset values in case they are constants, used in optimizations.
-//   int64_t limit_val;
-//   int64_t offset_val;
-//   //! The maximum amount of elements to emit
-//   unique_ptr<Expression> limit;
-//   //! The offset from the start to begin emitting elements
-//   unique_ptr<Expression> offset;
+  //! Limit and offset values in case they are constants, used in optimizations.
+  int64_t limit_val_;
+  int64_t offset_val_;
+  //! The maximum amount of elements to emit
+  unique_ptr<Expression> limit_;
+  //! The offset from the start to begin emitting elements
+  unique_ptr<Expression> offset_;
 
-//  public:
-//   vector<ColumnBinding> GetColumnBindings() override;
+ public:
+  vector<ColumnBinding> GetColumnBindings() override;
 
-//   uint64_t EstimateCardinality(ClientContext &context) override;
-
-//  protected:
-//   void ResolveTypes() override;
-// };
-// }  // namespace bustub
+ protected:
+  void ResolveTypes() override;
+};
+}  // namespace bustub
--- src/planner/operator/logical_limit.cpp
+++ src/planner/operator/logical_limit.cpp
@@ -1,17 +1,17 @@
-// #include "planner/operator/logical_limit.h"
+#include "planner/operator/logical_limit.h"
 
-// namespace bustub {
+namespace bustub {
 
-// LogicalLimit::LogicalLimit(int64_t limit_val, int64_t offset_val, unique_ptr<Expression> limit,
-//                            unique_ptr<Expression> offset)
-//     : LogicalOperator(LogicalOperatorType::LOGICAL_LIMIT),
-//       limit_val(limit_val),
-//       offset_val(offset_val),
-//       limit(move(limit)),
-//       offset(move(offset)) {}
+LogicalLimit::LogicalLimit(int64_t limit_val, int64_t offset_val, unique_ptr<Expression> limit,
+                           unique_ptr<Expression> offset)
+    : LogicalOperator(LogicalOperatorType::LOGICAL_LIMIT),
+      limit_val_(limit_val),
+      offset_val_(offset_val),
+      limit_(move(limit)),
+      offset_(move(offset)) {}
 
-// vector<ColumnBinding> LogicalLimit::GetColumnBindings() { return children[0]->GetColumnBindings(); }
+vector<ColumnBinding> LogicalLimit::GetColumnBindings() { return children_[0]->GetColumnBindings(); }
 
-// void LogicalLimit::ResolveTypes() { types = children[0]->types_; }
+void LogicalLimit::ResolveTypes() { types_ = children_[0]->types_; }
 
-// }  // namespace bustub
+}  // namespace bustub
